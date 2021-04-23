a849ced46d7313f688ab76859ac93ccdc3457027
Black4x <45229231+FurryBlackFox@users.noreply.github.com>
2021-04-23 17:48:38 +0300

    Implemented Canvas Safe Area Controller

---
+++ Assets/Scripts/UI/SafeAreaController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SafeAreaController : MonoBehaviour
+{
+    private CanvasScaler canvasScaler;
+    private float bottomUnits, topUnits;
+    void Start()
+    {
+        canvasScaler = GetComponentInParent<CanvasScaler>();
+        ApplyVerticalSafeArea();
+    }
+    
+        
+    private void ApplyVerticalSafeArea()
+    {
+        if(!Application.isMobilePlatform)
+            return;
+
+        var bottomPixels = Screen.safeArea.y;
+        var topPixel = Screen.currentResolution.height - (Screen.safeArea.y + Screen.safeArea.height);
+    
+        var bottomRatio = bottomPixels / Screen.currentResolution.height;
+        var topRatio = topPixel / Screen.currentResolution.height;
+    
+        var referenceResolution = canvasScaler.referenceResolution;
+        bottomUnits = referenceResolution.y * bottomRatio;
+        topUnits = referenceResolution.y * topRatio;
+    
+        var rectTransform = GetComponent<RectTransform>();
+        rectTransform.offsetMin = new Vector2(rectTransform.offsetMin.x, bottomUnits);
+        rectTransform.offsetMax = new Vector2(rectTransform.offsetMax.x, -topUnits);
+    }
+}
